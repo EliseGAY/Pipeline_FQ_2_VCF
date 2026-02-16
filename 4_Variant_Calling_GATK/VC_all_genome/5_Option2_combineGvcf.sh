9bcd3dd9d72c11a19a980abd030f39efab616a39
Elise J Gay <41110622+EliseGAY@users.noreply.github.com>
2026-02-16 10:11:30 +0100

    Enhance GATK script with parallelization and input updates
    Added parallelization options and improved input handling for GATK script.

--- 4_Variant_Calling_GATK/VC_all_genome/5_Option2_combineGvcf.sh
+++ 4_Variant_Calling_GATK/VC_all_genome/5_Option2_combineGvcf.sh
@@ -6,22 +6,41 @@
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=200G
 #SBATCH -V
+# IF YOU WANT PARALLELISE BY SCAFFOLD :
+#SBATCH --array=1-20%10
 
 # IMPORT MODULE
 # module load singularity/4.2.2 / if gatk not installed
 
-# INPUTS : 
-
+# INPUTS :
+# IF YOU WANT PARALLELISE BY SCAFFOLD :
+# Interval list file contains all contig/chr/scaffold you want to do the calling on. Put all your contig name if you want to do the calling in all the genome
+Intervall_list="/scratch/gaye/RADSeq_JC/ReferenceGenome/intervall.list"
 # File with one scaffold or chromosome per lines
-chr_list=$(cat chr.list)
+mapfile sc_list < <(cat $Intervall_list)
+idx=$((SLURM_ARRAY_TASK_ID-1))
+curren_sc="${sc_list[$idx]//[[:space:]]/}"
+echo ${curren_sc}
 
 # List of vcf file in the following format : --variant /PATH_TO_SAMPLE1/SAMPLE1_gatk.vcf.gz --variant  /PATH_TO_SAMPLE1/SAMPLE1_gatk.vcf.gz
 VCFs_File="/path_to/VCF_list.txt"
 Genome="PATH_TO/Genome.fasta" # index .fai and .dict have to be present in the same directory (see samtools faidx and gatk CreateSequenceDictionary functions)
 
-# Run GATK
+# Run GATK ON WHOLE GENOME (OPTION 1)
 Gatk --java --java-options "-Xmx200g" -jar CombineGVCFs \
 -R ${Genome} \
 --variant ${VCFs_File} \
-# -L $CHR \ : if you want to do that by chromosome or on a subset
 -O All.vcf.gz
+
+# RUN GATK BY SCAFFOLD (option 2) :
+output="${curren_sc}.vcf.gz"
+echo $output
+
+# Run GATK
+java "-Xmx200g" -jar $gatk CombineGVCFs \
+-R ${Genome} \
+-L ${curren_sc} \
+--variant ${VCFs_File} \
+-O ${output}
+
+
