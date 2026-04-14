5b7b03274466f03b202d8f1582c18b226a4def1a
Elise J Gay <41110622+EliseGAY@users.noreply.github.com>
2026-04-14 15:35:27 +0200

    Update 1_Filters_LowQual.sh

--- 5_gVCF_Filters/1_Filters_LowQual.sh
+++ 5_gVCF_Filters/1_Filters_LowQual.sh
@@ -10,7 +10,7 @@
 #SBATCH --mem-per-cpu=100G
 
 Input="/your/path/XXX.TAG.vcf.gz"
-
+VCF_OUT="/your/path/XXX.OUT.vcf.gz"
 #------------------------------------------------------------#
 # Filtering: Remove low-quality positions and unwanted variants
 # - Remove LowQual, MQFILTER, Repeat, and Indels
@@ -21,15 +21,39 @@ bcftools index VCF.TAG.vcf.gz
 
 # removed indel with AWK command
 bcftools view ${Input} -e 'FILTER~"MQFILTER|LowQual|Repeat" || TYPE="indel"' --threads 8 -Oz -o VCF.TAG.Flowqual_Noindels_Norepeat.vcf.gz
+bcftools index VCF.TAG.Flowqual_Noindels_Norepeat.vcf.gz
 
-# Example of filters on SNP (to keep snp only), missing data, and MAF and. To filter the DP and missing data along with custom vizualisation see the DP_Na_Filter R script.
+# Remove samples :
+bcftools view VCF.TAG.Flowqual_Noindels_Norepeat.vcf.gz  -s ^MysamplesName -Oz -o VCF.TAG.Flowqual_Noindels_Norepeat.Sampled.vcf.gz 
+bcftools index VCF.TAG.Flowqual_Noindels_Norepeat.Sampled.vcf.gz 
+
+# extract region 
+bcftools view VCF.TAG.Flowqual_Noindels_Norepeat.vcf.gz --threads 8 -r MyChr:start-end -Oz -o VCF.TAG.Flowqual_Noindels_Norepeat.subset.vcf.gz
+# or with a bed file
+bcftools view VCF.TAG.Flowqual_Noindels_Norepeat.vcf.gz --threads 8 -R Myfile.bed -Oz -o VCF.TAG.Flowqual_Noindels_Norepeat.subset.vcf.gz
+
+# Example of filters on SNP (to keep snp only), missing data, and MAF and. 
+# To filter the DP and missing data along with custom vizualisation see the DP_Na_Filter R script in VCF2PopStructure pipeline.
+
+# Keep only biallelic site :
+        # -m, --min-alleles INT
+        # print sites with at least INT alleles listed in REF and ALT columns
+        # -M, --max-alleles INT
+        # print sites with at most INT alleles listed in REF and ALT columns. Use -m2 -M2 -v snps to only view biallelic SNPs.
+
+bcftools view VCF.TAG.Flowqual_Noindels_Norepeat.vcf.gz  --threads 8 -m2 -M2 -v snps -Oz -o VCF.TAG.Flowqual_Noindels_Norepeat.vcf.gz.SNP.vcf.gz
+bcftools index VCF.TAG.Flowqual_Noindels_Norepeat.vcf.gz.SNP.vcf.g
 
 # keep the monomorpohic sites, the Bi-allelic sites and no NA
 bcftools view -M2 -g  ^miss -Oz -o ${VCF_OUT}
 
-# filter for NA :
+# filter only for NA :
 bcftools view -i 'F_MISSING<0.2' -o ${VCF_OUT} -Oz
+bvcftools index ${VCF_OUT}
 
-# filter for MAF :
+# filter only for MAF :
+bcftools view -i 'MAF>=0.5' -Oz -o ${VCF_OUT}
+bcftools index ${VCF_OUT}
 
-##TO DO
+# use plink to generate ped or other various extension
+vcftools --gzvcf ${Input} --out Prefix_Name --plink
